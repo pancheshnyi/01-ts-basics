002103790107aa61eceb398a1d02252296e08122
Oleg_Pancheshniy <oleg.pancheshniy@gmail.com>
2025-08-24 12:50:48 +0300

    homeWork1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-// import "./01-primitives";
-// import "./02-objects";
-// import "./03-arrays";
-// import "./04-types";
-import "./05-functions";
+// import "./task-1";
+// import "./task-2";
+// import "./task-3";
+// import "./task-4";
+// import "./task-5";
+// import "./task-6";
+// import "./task-7";
+import "./task-8";
